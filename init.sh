f4baea83692d8c5a9101719153392fef54f920fd
Bishal <bishal@gokwik.co>
2025-03-28 22:46:36 +0530

    baah

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-rm -rf ./dags/zips/
+rm *.zip
+rm -rf ./dags/zips/*
 zip -r spark_jobs.zip spark_jobs/
 cp -r spark_jobs* ./dags/zips/
--- spark_jobs/jobs/rill_clickhouse.py
+++ spark_jobs/jobs/rill_clickhouse.py
@@ -10,13 +10,7 @@ def main():
     text = "Hello Spark Hello Python Hello Airflow Hello Docker and Hello Yusuf"
     
     # Create RDD and perform word count
-    words = spark.sparkContext.parallelize(text.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-    
-    # Collect and print word counts
-    for word, count in wordCounts.collect():
-        print(f"{word}: {count}")
-    
+    words = spark.sparkContext.parallelize(text.split(" "))    
     # Stop the Spark session
     spark.stop()
 
