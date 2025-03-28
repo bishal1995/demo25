b8c060f362195bbbda7dd2a9a40685870a0912ce
Bishal <bishal@gokwik.co>
2025-03-28 23:08:59 +0530

    baah

--- dags/rill_pipeline.py
+++ dags/rill_pipeline.py
@@ -20,17 +20,21 @@ dag = DAG(
 )
 
 # Spark Submit Operator to execute the PySpark script
-spark_task = SparkSubmitOperator(
-    task_id="run_nyc_taxi_spark_job",
-    application="/opt/airflow/dags/spark_jobs.zip",
-    conn_id="spark_default",
+spark_submit_task = SparkSubmitOperator(
+    task_id='spark_submit_task',
+    application='/opt/airflow/dags/zips/spark_jobs/jobs/rill_clickhouse.py',
+    py_files='/opt/airflow/dags/zips/spark_jobs.zip',
+    conn_id='spark_default',  # Make sure to set up this connection in Airflow connections
+    total_executor_cores=4,  # equivalent to num-executors * executor-cores
     executor_cores=2,
-    executor_memory="2g",
-    driver_memory="2g",
-    num_executors=2,
-    application_args=["spark_jobs.jobs.nyc_taxi"],
-    conf={"spark.master": "spark://spark-master:7077"},
-    dag=dag,
+    executor_memory='2g',
+    driver_memory='2g',
+    name='arrow-spark',
+    application_args=[],  # Add any additional arguments if needed
+    spark_binary='spark-submit',
+    conf={
+        'spark.master': 'spark://spark-master:7077'
+    }
 )
 
 # Set the task execution order
